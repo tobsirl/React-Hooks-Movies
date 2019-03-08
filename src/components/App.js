ddc702ffe66fc8c9908ed6f85a3eaa0441a688d7
Paul Tobin <tobster111@gmail.com>
2019-03-08 10:07:24 +0000

    Refactored App.js to a funtional component

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 // Import the components
-import Header from "./Header";
-import Movie from "./Movie";
-import Search from "./Search";
+import Header from './Header';
+import Movie from './Movie';
+import Search from './Search';
 
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; 
+// OMDB API key
+const MOVIE_API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=8fc593f4';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Header</h2>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Header</h2>
+      </header>
+    </div>
+  );
+};
 
 export default App;
