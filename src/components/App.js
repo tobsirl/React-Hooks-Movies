10cb682d15a627c290495de2d3e119a641a9e83b
Paul Tobin <tobster111@gmail.com>
2019-03-08 10:11:30 +0000

    Added useState and useEffect to make the api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 // Import the components
@@ -10,6 +10,22 @@ import Search from './Search';
 const MOVIE_API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=8fc593f4';
 
 const App = () => {
+  // using new react hooks useState
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  // useEffect use to make the api call using fetch
+  // second argument make useEffect behave like componentDidMount
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
