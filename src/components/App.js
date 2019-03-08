41c8762576084e5a489caf72e35255b601576b4c
Paul Tobin <tobster111@gmail.com>
2019-03-08 10:05:04 +0000

    Added the Search component using useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Import the components
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; 
+
 class App extends Component {
   render() {
     return (
