75ed64a84008767cdb0f6b192bce04b1476fe12a
Paul Tobin <tobster111@gmail.com>
2019-03-10 17:21:48 +0000

    Added onClick to get the data on an individual movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,6 +3,18 @@ import React from 'react';
 const DEFAULT_PLACEHOLDER_IMAGE =
   'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
 
+// OMDB API key
+// const MOVIE_API_URL = 'https://www.omdbapi.com/?s=man&apikey=8fc593f4';
+
+const getMovie = id => {
+  console.log(id);
+  fetch(`https://www.omdbapi.com/?apikey=8fc593f4&i=${id}`)
+    .then(res => res.json())
+    .then(jsonRes => {
+      console.log(jsonRes);
+    });
+};
+
 const Movie = ({ movie }) => {
   const poster =
     movie.Poster === 'N/A' ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
@@ -14,6 +26,7 @@ const Movie = ({ movie }) => {
           width="200"
           alt={`The movie titled: ${movie.Title}`}
           src={poster}
+          onClick={getMovie.bind(this, movie.imdbID)}
         />
       </div>
       <p>({movie.Year})</p>
